eacc6ef085f1b46e4e9f9b67756523af7b140bd2
OktaAzr22 <oktaazhar22102005@gmail.com>
2025-10-30 14:27:53 +0700

    feat: add struktur

---
+++ app/Http/Controllers/UangSakuModelController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UangSakuModel;
+use Illuminate\Http\Request;
+
+class UangSakuModelController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(UangSakuModel $uangSakuModel)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(UangSakuModel $uangSakuModel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, UangSakuModel $uangSakuModel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(UangSakuModel $uangSakuModel)
+    {
+        //
+    }
+}
